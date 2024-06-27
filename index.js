c44b3b856f5dfda6360f8e67aa592416a2544781
code-iddih <idinyamwaya@gmail.com>
2024-06-27 22:01:03 +0300

    Completes the Tasks

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructively Adds a cat at the end of the Array
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Destructively Adds a cat at the beginning of the Array
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+// Destructively Removes the last Cat in the Array
+
+function destructivelyRemoveLastCat() {
+    cats.pop() ;
+}
+
+// Destructively Removes the last Cat in the Array
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Non-Destructively Adds a cat at the beginning of the Array
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+// Non-Destructively Adds a cat at the end of the Array
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+// Non-Destructively Removes a cat at the end of the Array
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+// Non-Destructively Removes a cat at the beginning of the Array
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
